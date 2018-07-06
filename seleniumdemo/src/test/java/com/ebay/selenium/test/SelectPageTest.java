485f6087212df9b138b9f323a4b25a35976d4845
Venkatesan Kannan <sharevenki@gmail.com>
2018-07-06 14:52:19 -0700

    multiselect example

--- seleniumdemo/src/test/java/com/ebay/selenium/test/SelectPageTest.java
+++ seleniumdemo/src/test/java/com/ebay/selenium/test/SelectPageTest.java
@@ -1,6 +1,7 @@
 package com.ebay.selenium.test;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -31,8 +32,36 @@ public class SelectPageTest {
 		WebElement element=seleniumDropDownDemo.getDropDownElement();
 		Select se=new Select(element);
 		se.selectByIndex(1);
+		se.selectByValue("Wednesday");
+		WebElement option = se.getFirstSelectedOption();
+		WebElement ResulteElement = seleniumDropDownDemo.getResultElement();
+		String result_text = ResulteElement.getText();
+		Assert.assertTrue(result_text.equals("Day selected :- "+option.getText()));
 		List<WebElement> l = se.getOptions();
 		Assert.assertTrue(l.size() > 0);
+	}
+	
+	@Test
+	public void multiSelectExample() {
+		driver.get("http://www.seleniumeasy.com/test/basic-select-dropdown-demo.html");
+		SeleniumDropDownDemo seleniumDropDownDemo = PageFactory.initElements(driver, SeleniumDropDownDemo.class);
+		WebElement element = seleniumDropDownDemo.getMultiSelectElement();
+		Select se=new Select(element);
+		se.selectByIndex(0);
+		se.selectByValue("Washington");
+		List<WebElement> l = se.getAllSelectedOptions();
+		StringBuilder sb = new StringBuilder();
+		for(WebElement el : l) {
+			sb.append(el.getText());
+			sb.append(",");
+		}
+		sb.setLength(Math.max(sb.length() - 1, 0));
+		
+		seleniumDropDownDemo.clickPrintAllElement();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);	
+		String result_text = seleniumDropDownDemo.getMultiSelectResultElement().getText();
+		
+		Assert.assertTrue(result_text.equals("Options selected are : "+sb.toString()));
 	}
 	
 	@AfterClass
